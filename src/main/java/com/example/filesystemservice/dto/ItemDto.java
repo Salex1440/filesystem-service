c7441d87307e4906d2b6c45968af47f13fd7a676
salexsh <ShutovAlexandr@yandex.ru>
2022-09-10 21:42:03 +0300

    Created dto classes

---
+++ src/main/java/com/example/filesystemservice/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.example.filesystemservice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ItemDto {
+
+    private String type;
+
+    private String url;
+
+    private String id;
+
+    private String parentId;
+
+    private int size;
+
+}
