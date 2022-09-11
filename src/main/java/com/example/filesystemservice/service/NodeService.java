3934e744027fd2e955672226a9ae0d48d10e9115
salexsh <ShutovAlexandr@yandex.ru>
2022-09-11 12:32:21 +0300

    Added checks

--- src/main/java/com/example/filesystemservice/service/NodeService.java
+++ src/main/java/com/example/filesystemservice/service/NodeService.java
@@ -2,6 +2,7 @@ package com.example.filesystemservice.service;
 
 import com.example.filesystemservice.dto.BatchDto;
 import com.example.filesystemservice.dto.ItemDto;
+import com.example.filesystemservice.exception.BadRequestException;
 import com.example.filesystemservice.exception.UnprocessableEntityException;
 import com.example.filesystemservice.repository.Node;
 import com.example.filesystemservice.repository.NodeRepository;
@@ -20,7 +21,10 @@ public class NodeService {
             if (node == null) {
                 if (item.getType().equals(NodeType.FOLDER.toString()) && item.getUrl() != null) {
                     throw new UnprocessableEntityException("For a node of type \"FOLDER\" URL must be NULL!");
+                } else if (item.getType().equals((NodeType.FILE).toString()) && item.getUrl().length() > 255) {
+                    throw new BadRequestException("URL size must be less or equal than 255!");
                 }
+
                 node.setId(item.getId());
                 node.setType(item.getType());
                 node.setUrl(item.getUrl());
@@ -36,6 +40,16 @@ public class NodeService {
                 if (!node.getType().equals(item.getType())) {
                     throw new UnprocessableEntityException("Can't change type of the item!");
                 }
+                if (item.getType().equals(NodeType.FOLDER.toString()) && item.getUrl() != null) {
+                    throw new UnprocessableEntityException("For a node of type \"FOLDER\" URL must be NULL!");
+                } else if (item.getType().equals((NodeType.FILE).toString()) && item.getUrl().length() > 255) {
+                    throw new BadRequestException("URL size must be less or equal than 255!");
+                }
+                node.setUrl(item.getUrl());
+                node.setDate(updateDate);
+                node.setSize(item.getSize());
+                node.setParentId(item.getParentId());
+                nodeRepository.save(node);
             }
         }
     }
