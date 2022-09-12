c0fb83079de4a7b0d0b163587bea1953198df6a6
Salex <123@13.com>
2022-09-12 14:33:27 +0300

    Created MainController class.

---
+++ src/main/java/com/example/filesystemservice/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.example.filesystemservice.controller;
+
+import com.example.filesystemservice.dto.NodeDto;
+import com.example.filesystemservice.service.NodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/")
+public class MainController {
+    @Autowired
+    NodeService nodeService;
+
+    @PostMapping(path = "imports")
+    @ResponseBody
+    public String imports(@RequestBody String batch) {
+        nodeService.importBatch(batch);
+        return "";
+    }
+
+    @DeleteMapping(path = "delete")
+    @ResponseBody
+    public String delete(@PathVariable String id) {
+        nodeService.deleteNodeById(id);
+        return "";
+    }
+
+    @GetMapping(path = "nodes")
+    @ResponseBody
+    public NodeDto nodes(@PathVariable String id) {
+        return nodeService.getNodeById(id);
+    }
+
+}
